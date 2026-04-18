25cd8b8596f6748e572643c4d9ddd292afc621c8
Alasela Babatunde <alaselababatunde10@gmail.com>
2026-04-18 23:23:22 +0100

    Physically delete middleware.ts to force Vercel to stop invocation

--- middleware.ts
+++
@@ -1,4 +0,0 @@
-// Middleware removed as requested to resolve persistent Vercel 500 errors.
-// Automatic page protection is now disabled.
-export function middleware() {}
-export const config = { matcher: [] }
